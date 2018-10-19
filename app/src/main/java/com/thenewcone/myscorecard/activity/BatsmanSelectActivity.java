f8d1fe0682cf687a3cc8eb9b002fff8994612dde
Anil Kumar Madugula <anil1804@gmail.com>
2018-10-19 16:59:31 +0530

    - Effected By Functionality Draft Complete

---
+++ app/src/main/java/com/thenewcone/myscorecard/activity/BatsmanSelectActivity.java
@@ -0,0 +1,139 @@
+package com.thenewcone.myscorecard.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.thenewcone.myscorecard.R;
+import com.thenewcone.myscorecard.adapter.BatsmanListAdapter;
+import com.thenewcone.myscorecard.intf.ItemClickListener;
+import com.thenewcone.myscorecard.player.BatsmanStats;
+import com.thenewcone.myscorecard.player.Player;
+import com.thenewcone.myscorecard.utils.CommonUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BatsmanSelectActivity extends Activity
+        implements ItemClickListener, View.OnClickListener {
+
+    public static final String ARG_PLAYER_LIST = "PlayerList";
+    public static final String ARG_BATSMAN_LIST = "BatsmanList";
+    public static final String ARG_SEL_BATSMAN = "SelectedBatsman";
+
+    public static final int RESP_CODE_OK = 1;
+    public static final int RESP_CODE_CANCEL = -1;
+
+    List<BatsmanStats> dispBatsmen;
+    int currBattingPosn= -1;
+    @Override
+    public void onBackPressed() {
+        //
+    }
+
+    BatsmanStats[] batsmen;
+    Player[] players;
+    BatsmanStats selBatsman;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_select);
+
+        RecyclerView rcvPlayerList = findViewById(R.id.rcvPlayerList);
+        rcvPlayerList.setHasFixedSize(false);
+
+        findViewById(R.id.btnSelPlayerOK).setOnClickListener(this);
+        findViewById(R.id.btnSelPlayerCancel).setOnClickListener(this);
+
+        Intent intent = getIntent();
+        if(intent != null) {
+            players = CommonUtils.objectArrToPlayerArr((Object[]) intent.getSerializableExtra(ARG_PLAYER_LIST));
+            batsmen = CommonUtils.objectArrToBatsmanArr((Object[]) intent.getSerializableExtra(ARG_BATSMAN_LIST));
+
+            dispBatsmen = getDisplayBatsmanList(players, batsmen);
+        }
+
+        if(dispBatsmen != null && dispBatsmen.size() > 0) {
+            rcvPlayerList.setLayoutManager(new LinearLayoutManager(this));
+            BatsmanListAdapter adapter = new BatsmanListAdapter(this, dispBatsmen, currBattingPosn);
+            adapter.setClickListener(this);
+            rcvPlayerList.setAdapter(adapter);
+
+            LinearLayoutManager llm = new LinearLayoutManager(this);
+            llm.setOrientation(LinearLayoutManager.VERTICAL);
+            rcvPlayerList.setLayoutManager(llm);
+
+            DividerItemDecoration itemDecor = new DividerItemDecoration(rcvPlayerList.getContext(), DividerItemDecoration.HORIZONTAL);
+            rcvPlayerList.addItemDecoration(itemDecor);
+
+            rcvPlayerList.setItemAnimator(new DefaultItemAnimator());
+        }
+    }
+
+    private List<BatsmanStats> getDisplayBatsmanList(Player[] players, BatsmanStats[] batsmenPlayed) {
+        List<BatsmanStats> dispBatsmen = new ArrayList<>();
+
+        dispBatsmen.addAll(Arrays.asList(batsmenPlayed));
+
+        List<String> playedBatsmen;
+        if(players != null && players.length > 0) {
+            playedBatsmen = getPlayedBatsmen(batsmenPlayed);
+            currBattingPosn = playedBatsmen.size() + 1;
+
+            for(Player player : players) {
+                if(!playedBatsmen.contains(player.getName())) {
+                    dispBatsmen.add(new BatsmanStats(player, currBattingPosn));
+                }
+            }
+        } else {
+            currBattingPosn = -1;
+        }
+
+        return dispBatsmen;
+    }
+
+    private List<String> getPlayedBatsmen(BatsmanStats[] batsmenPlayed) {
+        List<String> playedPlayers = new ArrayList<>();
+
+        for(BatsmanStats batsman : batsmenPlayed)
+            playedPlayers.add(batsman.getBatsmanName());
+
+        return playedPlayers;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent respIntent = getIntent();
+
+        switch (view.getId()) {
+            case R.id.btnSelPlayerOK:
+                if(selBatsman == null)
+                    Toast.makeText(this, "Batsman not selected", Toast.LENGTH_SHORT).show();
+                else {
+                    respIntent.putExtra(ARG_SEL_BATSMAN, selBatsman);
+                    setResult(RESP_CODE_OK, respIntent);
+                    finish();
+                }
+                break;
+
+            case R.id.btnSelPlayerCancel:
+                setResult(RESP_CODE_CANCEL);
+                finish();
+                break;
+
+        }
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        selBatsman = dispBatsmen.get(position);
+    }
+}
---
+++ app/src/main/java/com/thenewcone/myscorecard/adapter/BatsmanListAdapter.java
@@ -0,0 +1,98 @@
+package com.thenewcone.myscorecard.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.thenewcone.myscorecard.R;
+import com.thenewcone.myscorecard.intf.ItemClickListener;
+import com.thenewcone.myscorecard.player.BatsmanStats;
+
+import java.util.List;
+
+public class BatsmanListAdapter extends RecyclerView.Adapter<BatsmanListAdapter.MyViewHolder> {
+
+	private List<BatsmanStats> batsmen;
+	private Context context;
+	private ItemClickListener clickListener;
+	private int selectedIndex = -1;
+	private int newBatsmanPosn = -1;
+
+	public BatsmanListAdapter(@NonNull Context context, @NonNull List<BatsmanStats> batsmen, int position) {
+		this.context = context;
+		this.batsmen = batsmen;
+		this.newBatsmanPosn = position;
+	}
+
+	@NonNull
+	@Override
+	public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		View playerView = LayoutInflater.from(context).inflate(R.layout.activity_batsman_list_item, parent, false);
+
+		return new MyViewHolder(playerView);
+	}
+
+	@Override
+	public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
+        BatsmanStats batsman = batsmen.get(position);
+		myViewHolder.setData(batsman);
+
+		if(selectedIndex == position)
+			myViewHolder.llBatsmanItem.setSelected(true);
+		else
+			myViewHolder.llBatsmanItem.setSelected(false);
+	}
+
+	@Override
+	public int getItemCount() {
+		return batsmen.size();
+	}
+
+	public void setClickListener(ItemClickListener itemClickListener) {
+		this.clickListener = itemClickListener;
+	}
+
+	public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+		TextView tvBatsmanName, tvBattingStyle;
+		LinearLayout llBatsmanItem;
+
+		private MyViewHolder(@NonNull View itemView) {
+			super(itemView);
+
+            tvBatsmanName = itemView.findViewById(R.id.tvBatsmanName);
+            tvBattingStyle = itemView.findViewById(R.id.tvBattingStyle);
+			llBatsmanItem = itemView.findViewById(R.id.llBatsmanItem);
+			llBatsmanItem.setEnabled(true);
+            itemView.setOnClickListener(this);
+		}
+
+		public void setData(BatsmanStats batsman) {
+            tvBatsmanName.setText(batsman.getBatsmanName());
+
+             if(newBatsmanPosn > -1) {
+                 tvBattingStyle.setText(batsman.getPlayer().getBattingStyle().toString());
+             }
+
+            if (newBatsmanPosn > batsman.getPosition()) {
+                llBatsmanItem.setEnabled(false);
+            }
+		}
+
+
+		@Override
+		public void onClick(View view)
+		{
+			notifyItemChanged(selectedIndex);
+			selectedIndex = getAdapterPosition();
+			notifyItemChanged(selectedIndex);
+
+			if(clickListener != null)
+			    clickListener.onItemClick(view, selectedIndex);
+		}
+	}
+}
