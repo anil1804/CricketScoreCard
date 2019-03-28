950f8ffa8f107c0bee2e29a1040ebcb734ff53c3
d257728 (Anil) <Anil_Madugula@infosys.com>
2019-03-28 15:41:18 +0530

    - Better controlled match testing - Upgraded for various types of Round-Robin

---
+++ app/src/androidTest/java/com/theNewCone/cricketScoreCard/utils/TeamInfo.java
@@ -0,0 +1,35 @@
+package com.theNewCone.cricketScoreCard.utils;
+
+import com.theNewCone.cricketScoreCard.match.Team;
+
+class TeamInfo {
+	private final Team team;
+	private String captain, wicketKeeper;
+	private String[] players;
+
+	TeamInfo(Team team) {
+		this.team = team;
+	}
+
+	String getCaptain() {
+		return captain;
+	}
+
+	String getWicketKeeper() {
+		return wicketKeeper;
+	}
+
+	String[] getPlayers() {
+		return players;
+	}
+
+	void setPlayerInfo(String[] players, String captain, String wicketKeeper) {
+		this.players = players;
+		this.captain = captain;
+		this.wicketKeeper = wicketKeeper;
+	}
+
+	Team getTeam() {
+		return team;
+	}
+}
